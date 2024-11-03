2d3cf0468509142a9ae47e8a239978bf04a0b035
Mattie Velasquez <tethergirl30@yahoo.com>
2024-11-02 22:18:45 -0600

    wrap link trial 1

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ projects = [] }) => {
     <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 justify-between">
       {projects.map((project, index) => (
         <div
-          className="flex mx-auto flex-col max-w-[350px] w-full h-full project-card py-4 px-8 bg-white hover:bg-zinc-100 dark:bg-zinc-900 dark:hover:bg-zinc-800 rounded-md gap-1"
+          className="flex mx-auto flex-col max-w-[340px] w-full h-full project-card py-4 px-8 bg-white hover:bg-zinc-100 dark:bg-zinc-900 dark:hover:bg-zinc-800 rounded-md gap-1"
           key={index}
         >
           <Image
@@ -18,11 +18,11 @@ const ProjectCard = ({ projects = [] }) => {
           />
           <div className="w-[276px] flex flex-col gap-4">
             <h2 className="leading-7 font-semibold text-zinc-800 dark:text-zinc-100">{project.name}</h2>
-            <p className="w-full font-inter text-sm line-clamp-3 leading-6 text-zinc-700 dark:text-zinc-400">
+            <p className="w-[276px] font-inter text-sm line-clamp-3 leading-6 text-zinc-700 dark:text-zinc-400">
               {project.content}
             </p>
             <a
-              className="flex-row items-center leading-7 hover:underline text-zinc-800 dark:text-zinc-300"
+              className="mt-2 flex items-center justify-center px-4 py-2 hover:underline text-zinc-800 dark:text-zinc-300"
               href={project.link}
               target="_blank"
               rel="noreferrer"
